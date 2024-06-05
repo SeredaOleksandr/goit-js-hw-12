5ffa8a6109ecddf1eaafbddbd52fc35ecffd113a
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-06-05 19:51:51 +0300

    Finished hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,23 @@
-const userGallery = userSearch => {
-  const searchParams = new URLSearchParams({
-    key: '43654443-0aa2ac279d0400a8e119738dd',
-    q: userSearch,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
+import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export const perPage = 15;
+const API_KEY = '43654443-0aa2ac279d0400a8e119738dd';
+
+const userGallery = async (userSearch, myPage) => {
+  const response = await axios.get('https://pixabay.com/api', {
+    params: {
+      key: API_KEY,
+      q: userSearch,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: perPage,
+      page: myPage,
+    },
   });
 
-  const url = `https://pixabay.com/api/?${searchParams}`;
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return response.data;
 };
 
 export default userGallery;
--- src/main.js
+++ src/main.js
@@ -1,26 +1,102 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import userGallery from "./js/pixabay-api";
-import addImages from "./js/render-functions";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import userGallery from './js/pixabay-api';
+import addImages from './js/render-functions';
+import { perPage } from './js/pixabay-api';
+
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.js-load-more');
+let myPage = 1;
+let userSearch;
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const userSearch = event.target.elements.search.value.trim();
-    loader.style.display = 'flex';
-    gallery.innerHTML = '';
-    userGallery(userSearch)
-        .then(response => {
-            if (response.hits.length === 0) {
-                iziToast.show({
-                    message: 'Sorry, there are no images matching your search query. Please try again!'
-                })
-            }
-
-            addImages(response)
-        })
-        .catch(error => console.error('Error', error))
-        .finally(() => loader.style.display = 'none');
+iziToast.settings({
+  progressBar: false,
+  timeout: 4000,
+  resetOnHover: true,
+  icon: 'material-icons',
+  transitionIn: 'flipInX',
+  transitionOut: 'flipOutX',
+  position: 'topRight',
 });
+
+const showLoader = () => loader.classList.remove('is-hidden');
+const hideLoader = () => loader.classList.add('is-hidden');
+const showBtn = () => loadMoreBtn.classList.remove('is-hidden');
+const hideBtn = () => loadMoreBtn.classList.add('is-hidden');
+
+const onSubmit = async event => {
+  event.preventDefault();
+  userSearch = event.target.elements.search.value.trim();
+  if (!userSearch) return;
+  showLoader();
+  hideBtn();
+  gallery.innerHTML = '';
+
+  try {
+    myPage = 1;
+    const response = await userGallery(userSearch, myPage);
+    if (response.hits.length === 0) {
+      hideBtn();
+      iziToast.show({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    } else {
+      addImages(response);
+      if (response.totalHits > perPage) {
+        showBtn();
+      }
+    }
+  } catch (error) {
+    iziToast.error({
+      message: `Error: ${error.message}`,
+    });
+  } finally {
+    event.target.reset();
+    hideLoader();
+  }
+};
+
+const onClick = async () => {
+  myPage += 1;
+
+  try {
+    const response = await userGallery(userSearch, myPage);
+    const totalPages = Math.ceil(response.totalHits / perPage);
+
+    showLoader();
+    addImages(response);
+    scroll();
+
+    if (myPage >= totalPages) {
+      hideBtn();
+      iziToast.error({
+        position: 'topRight',
+        message: "We're sorry, there are no more posts to load",
+      });
+    }
+  } catch (error) {
+    iziToast.error({
+      message: `Error: ${error.message}`,
+    });
+  } finally {
+    hideLoader();
+  }
+};
+
+form.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onClick);
+
+const scroll = () => {
+  const galleryItem = document.querySelector('.gallery-item');
+  if (galleryItem) {
+    const itemHeight = galleryItem.getBoundingClientRect().height;
+    window.scrollBy({
+      top: itemHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+};
